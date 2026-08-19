31b566af3610f0fca3fd10c7be396f82a3d690c8
rishabhBudhouliya <rb5727@nyu.edu>
2026-08-19 13:34:26 -0400

    First pass works: kv can read a file and construct an in memory hashmap with the key values stored in the file
    - used libc::open, read, fstat syscalls
    - learnt about as_ptr() to translate a reference to a buffer
    - using split based on a predicate
    - the current design loads the entire file into memory during init of keyvalue server

--- key-value/src/main.rs
+++ key-value/src/main.rs
@@ -6,78 +6,74 @@ use libc;
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::ffi::c_void;
+use std::mem::MaybeUninit;
+use std::str::from_utf8;
 
-struct KeyValue<'a> {
-    capacity: u32,
+mod test;
+
+#[derive(Debug)]
+struct KeyValue {
     fd: u32,
-    buffer: &'a mut [u8],
     map: HashMap<String, u32>,
 }
 
-impl KeyValue<'_> {
-    pub fn new(capacity: u32, filename: String) -> Self {
+impl KeyValue {
+    pub fn new(filename: String) -> Self {
         let c_filename = CString::new(filename).expect("Failed to create CString");
         let fd = unsafe {
             libc::open(
                 c_filename.as_ptr(),
-                libc::O_CREAT | libc::O_APPEND | libc::O_EXCL,
-                libc::O_RDWR,
-            ) as u32
+                libc::O_RDWR | libc::O_CREAT | libc::O_APPEND,
+                0o666,
+            ) as i32
         };
-        let map = parse_buffer(fd);
+        if fd == -1 {
+            panic!("couldn't open fileserver");
+        }
+        let map = parse_buffer(fd as u32);
         Self {
-            capacity,
-            fd,
+            fd: (fd as u32),
             map,
-            buffer: &mut [],
         }
     }
-    // keeping the key data type to be strictly a string for now
-    // pub fn get(&self, key: String) -> u8 {
-    //     let chunk = 1024;
-    //     let mut buffer_start = self.buffer.as_ptr();
-    //     while true {
-    //         let read_bytes =
-    //             unsafe { libc::read(self.fd as i32, buffer_start as *mut c_void, chunk) };
-    //         if read_bytes == 0 {
-    //             break;
-    //         }
-    //         buffer_start = self.buffer[chunk..];
-    //         chunk += 1024;
-    //     }
-    // }
-
-    // "key:value/nkey:value/nkey:value"
 }
 
 fn parse_buffer(file_d: u32) -> HashMap<String, u32> {
-    let mut chunk = 4096;
-    let buffer = &mut [];
-    let mut buffer_start: *const u8 = buffer.as_ptr();
+    let mut stat: MaybeUninit<libc::stat> = MaybeUninit::uninit();
+    let ret = unsafe { libc::fstat(file_d as i32, stat.as_mut_ptr()) };
+    let size = if ret != -1 {
+        unsafe { stat.assume_init() }.st_size
+    } else {
+        0
+    };
+    let buffer = vec![0; size as usize];
+    let buffer_start: *const u8 = buffer.as_ptr();
     let mut map: HashMap<String, u32> = HashMap::new();
-    while true {
-        let read_bytes = unsafe { libc::read(file_d as i32, buffer_start as *mut c_void, chunk) };
-        if read_bytes == 0 {
+    let read_bytes =
+        unsafe { libc::read(file_d as i32, buffer_start as *mut c_void, size as usize) };
+    dbg!(read_bytes);
+    if read_bytes == 0 {
+        panic!("Couldn't read anything");
+    }
+    let iter = buffer.split(|num| *num == b'\n');
+    // this should give us a list of records
+    // [100, 201, 123, 212, 214, :, 144, 124, 412, 421]
+    for record in iter {
+        if record.is_empty() {
+            print!("Encountered an empty record");
             break;
         }
-        let mut iter = buffer.split(|num| *num == b'\n');
-        // this should give us a list of records
-        for record in iter {
-            let mut key_value_iter = record.split(|num| *num == b':');
-            // add a check on whether the iter has length = 2 or not
-            let key = key_value_iter.next().unwrap();
-            let value = key_value_iter.next().unwrap();
-            map.insert(
-                u32::from_be_bytes(key.try_into().unwrap()).to_string(),
-                u32::from_be_bytes(value.try_into().unwrap()),
-            );
-        }
-        buffer_start = buffer[chunk..].as_ptr();
-        chunk += 1024;
+        let mut key_value_iter = record.split(|num| *num == b':');
+        // add a check on whether the iter has length = 2 or not
+        let key = String::from_utf8((key_value_iter.next().unwrap()).try_into().unwrap()).unwrap();
+        let value = str::from_utf8(key_value_iter.next().unwrap()).unwrap();
+        map.insert(key, u32::from_str_radix(value, 10).unwrap());
     }
     return map;
 }
 
 fn main() {
+    let server = KeyValue::new("sample.txt".try_into().unwrap());
+    dbg!(server);
     println!("Hello, world!");
 }
