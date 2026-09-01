71faa2cd3d1e9e1c3257e0996ec344676c16a233
rishabhBudhouliya <rb5727@nyu.edu>
2026-09-01 12:28:43 -0400

    Make a single process database into an actual server
    - use unix domain sockets to talk to the server
    - introduce enums for byte parsing logic
    - establish a contract to communicate with the server
    - implement the listen function

--- key-value/src/main.rs
+++ key-value/src/main.rs
@@ -13,6 +13,42 @@ use std::str::FromStr;
 
 mod test;
 
+/*
+* WireFormat {
+    length,
+    Op,
+    value,
+    key,
+}
+*/
+#[repr(u8)]
+#[derive(Debug)]
+enum Op {
+    GET = 1,
+    SET = 2,
+    DELETE = 3,
+}
+
+#[derive(Debug)]
+enum Request<'a> {
+    GET { key: &'a str },
+    SET { key: String, value: u32 },
+    DELETE { key: &'a str },
+}
+
+impl TryFrom<u8> for Op {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Op::GET),
+            2 => Ok(Op::SET),
+            3 => Ok(Op::DELETE),
+            _ => Err(()),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct KeyValue {
     // mapping of filename to fd
@@ -33,6 +69,71 @@ impl KeyValue {
             file_map,
             map,
         }
+    }
+
+    pub fn listen(&self) {
+        let socket_fd = unsafe { libc::socket(libc::AF_UNIX, libc::SOCK_STREAM, 0) };
+        let mut sockaddr: libc::sockaddr_un = unsafe { std::mem::zeroed() };
+        let path = String::from("server.sock");
+        let c_path = CString::new(path).expect("Failed to create CString");
+        let mut counter = 0;
+        let s_bytes_arr = c_path.as_bytes();
+        for ele in sockaddr.sun_path.iter_mut() {
+            if counter >= s_bytes_arr.len() {
+                break;
+            }
+            *ele = s_bytes_arr[counter] as i8;
+            counter += 1;
+        }
+        sockaddr.sun_family = libc::AF_UNIX as u8;
+        // now that sockaddrr is ready, let's invoke the bind syscall
+        let raw_sock_ptr: *const libc::sockaddr =
+            (&sockaddr as *const libc::sockaddr_un) as *const libc::sockaddr;
+
+        let ret = unsafe {
+            libc::bind(
+                socket_fd,
+                raw_sock_ptr,
+                std::mem::size_of::<libc::sockaddr_un>() as u32,
+            )
+        };
+
+        if ret == -1 {
+            let err = io::Error::last_os_error();
+            dbg!(err);
+            println!("unable to bind to socket");
+        }
+
+        // start listening, keep the queue for backlog 0
+        let ret = unsafe { libc::listen(socket_fd, 0) };
+        if ret == -1 {
+            let err = io::Error::last_os_error();
+            dbg!(err);
+            println!("unable to start listening to socket");
+        }
+        // accept() blocks the caller until a connection is present unless non blocking sockets are used
+        let accepted_fd_socket =
+            unsafe { libc::accept(socket_fd, std::ptr::null_mut(), std::ptr::null_mut()) };
+        if accepted_fd_socket == -1 {
+            let err = io::Error::last_os_error();
+            dbg!(err);
+            println!("unable to accept connections");
+        }
+        let listen_buff = vec![0; 1024 as usize];
+        let listen_buff_ptr = listen_buff.as_ptr();
+
+        let num =
+            unsafe { libc::recv(accepted_fd_socket, listen_buff_ptr as *mut c_void, 1024, 0) };
+        if num == 0 {
+            println!("received no data from the client")
+        } else {
+            println!(
+                "received some data: {}",
+                str::from_utf8(&listen_buff[0..50]).unwrap()
+            )
+        }
+        let request = parse_request(&listen_buff);
+        println!("{request:?}")
     }
 
     // let's use the fs scan dir function
@@ -116,6 +217,53 @@ impl KeyValue {
         let _ = unsafe { libc::write(fd as i32, ptr as *mut c_void, size_buffer as usize) };
         // separate the key value insertion from file write
         self.map.insert(key, value);
+    }
+}
+// this takes in the byte stream and checks the op to figure out what to do with the request
+fn parse_request(buffer: &Vec<u8>) -> Request {
+    // this function should only person read operations on the original buffer
+    // format: lengthopcodevaluekey
+    let (len, rest) = buffer
+        .split_first_chunk::<4>()
+        .expect("buffer split at [length] failed");
+    let length = u32::from_be_bytes(*len);
+    let (op_byte, payload) = rest
+        .split_first_chunk::<1>()
+        .expect("buffer split at op failed");
+    let op = Op::try_from(op_byte[0]).expect("unable to parse op byte code");
+    let request: Request = match op {
+        Op::GET => parse_get(payload, length),
+        Op::SET => parse_set(payload, length),
+        Op::DELETE => parse_delete(payload, length),
+    };
+    request
+}
+
+fn parse_get(buffer: &[u8], length: u32) -> Request {
+    let ptx = 0;
+    let key = buffer[ptx..ptx + (length as usize)].try_into().unwrap();
+    Request::GET {
+        key: (str::from_utf8(key).unwrap()),
+    }
+}
+
+fn parse_set(buffer: &[u8], length: u32) -> Request {
+    let mut ptx = 0;
+    let value = u32::from_be_bytes(buffer[ptx..ptx + 4].try_into().unwrap());
+    ptx += 4;
+    let key = str::from_utf8(buffer[ptx..ptx + (length as usize)].try_into().unwrap());
+    Request::SET {
+        key: (String::from_str(key.unwrap()).unwrap()),
+        value: (value),
+    }
+    // return key, value
+}
+
+fn parse_delete(buffer: &[u8], lenght: u32) -> Request {
+    let mut ptx = 0;
+    let key = buffer[ptx..ptx + (lenght as usize)].try_into().unwrap();
+    Request::DELETE {
+        key: (str::from_utf8(key).unwrap()),
     }
 }
 
@@ -211,6 +359,8 @@ fn main() {
     }
     server.set(String::from("rishabh4"), 89);
     let all_keys = server.get_all();
+
+    server.listen();
     dbg!(all_keys);
     // server.delete_key("rishabh4");
     dbg!(server);
